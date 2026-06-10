e187a49a056a4c27e3799426c40880e3881676f4
Md Zahera <mdzaheera01@gmail.com>
2026-06-10 14:28:33 +0530

    Add files via upload

---
+++ CodeAlpha_ObjectDetection/app.py
@@ -0,0 +1,28 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolov8s.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model(frame,conf=0.5)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow(
+        "Object Detection",
+        annotated_frame
+    )
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
